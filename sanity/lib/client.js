eaeebae61228c8c063e0079c5302fa9bb0dd8128
Sharad Kalavadia <sharadkalavadia@gmail.com>
2025-06-07 13:39:54 +0530

    Initial commit

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: '6oyttiv3',
+  dataset: 'jb_prints',
   apiVersion: '2025-06-02', // Use 'latest' instead of a date string
   useCdn: true,
 })
