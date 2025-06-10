40f1e1f4aeb1b0e5556b3ade7dc62840d513c911
Sharad Kalavadia <sharadkalavadia@gmail.com>
2025-06-10 20:46:51 +0530

    Your commit message

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -3,6 +3,8 @@ import { createClient } from 'next-sanity'
 export const client = createClient({
   projectId: '6oyttiv3',
   dataset: 'jb_prints',
-  apiVersion: '2025-06-02', // Use 'latest' instead of a date string
+  apiVersion: '2025-06-10', // Updated to current date
   useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN, // Add token if you have one
+  ignoreBrowserTokenWarning: true, // Add this to prevent token warnings
 })
